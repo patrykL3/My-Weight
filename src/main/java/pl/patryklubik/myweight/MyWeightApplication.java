db78bd7fb8929da944c18aaf20f2a84121187f9b
Patryk Łubik <patryk.lubik@wp.pl>
2021-09-03 13:14:44 +0200

    skeleton

---
+++ src/main/java/pl/patryklubik/myweight/MyWeightApplication.java
@@ -0,0 +1,13 @@
+package pl.patryklubik.myweight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWeightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyWeightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/patryklubik/myweight/MyWeightApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.patryklubik.myweight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWeightApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
