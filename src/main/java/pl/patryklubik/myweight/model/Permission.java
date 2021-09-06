219f5f8923c936aadffdf6988080e6bade4a1979
Patryk Łubik <patryk.lubik@wp.pl>
2021-09-06 18:41:56 +0200

    permission model

--- src/main/java/pl/patryklubik/myweight/model/Permission.java
+++ src/main/java/pl/patryklubik/myweight/model/Permission.java
@@ -1,25 +1,21 @@
 package pl.patryklubik.myweight.model;
 
-import javax.persistence.*;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
-import java.util.Set;
 
 
 /**
  * Create by Patryk Łubik on 05.09.2021.
  */
-
-@Entity
-@Table(name = "roles")
-public class Role {
+public class Permission {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     @NotBlank
     private String name;
-    @OneToMany
-    private Set<Permission> permissions;
 
     public int getId() {
         return id;
