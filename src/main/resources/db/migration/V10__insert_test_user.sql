b4b681d5fe333350eb4bc634dd2293da65f2220a
Patryk Łubik <patryk.lubik@wp.pl>
2021-09-14 18:50:33 +0200

    basic data inserted

---
+++ src/main/resources/db/migration/V10__insert_test_user.sql
@@ -0,0 +1,2 @@
+
+INSERT INTO users VALUES (1, 'user', '$2a$10$bXxwM0f9wKrIA17FAZTaleB7zGXlnaokUxc/F.JxQBWGqrbqYFcUu', 1, 1);
---
+++ src/main/resources/db/migration/V9__connect_permissions_with_roles.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO roles_permissions VALUES (1, 1);
+INSERT INTO roles_permissions VALUES (1, 2);
+INSERT INTO roles_permissions VALUES (1, 3);
