a8259070ec1e5b9aa91242c20eda3662a7c7c75c
Patryk Łubik <patryk.lubik@wp.pl>
2021-10-17 11:17:29 +0200

    automatic date filling

---
+++ src/main/resources/static/js/automatic-date-filling.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    let date = new Date();
+
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+
+    if (month < 10) month = "0" + month;
+    if (day < 10) day = "0" + day;
+
+    let today = year + "-" + month + "-" + day;
+
+    if ($('#date').attr("value") == "") {
+        $('#date').attr("value", today);
+    }
+});
