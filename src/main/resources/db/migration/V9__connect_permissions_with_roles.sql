b0a130647a58293c576360f437a9cdc50881e83b
Patryk Łubik <patryk.lubik@wp.pl>
2021-11-01 17:29:19 +0100

    adding permissions

--- src/main/resources/db/migration/V9__connect_permissions_with_roles.sql
+++ src/main/resources/db/migration/V9__connect_permissions_with_roles.sql
@@ -2,3 +2,5 @@
 INSERT INTO roles_permissions VALUES (1, 1);
 INSERT INTO roles_permissions VALUES (1, 2);
 INSERT INTO roles_permissions VALUES (1, 3);
+INSERT INTO roles_permissions VALUES (1, 4);
+INSERT INTO roles_permissions VALUES (1, 5);
